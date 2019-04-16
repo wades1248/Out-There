93452e5f27a3430cf008b1cbedf3fbcfe897f20a
wades1248 <wades1248@gmail.com>
2019-04-15 22:46:54 -0500

    getting very close, added most CSS

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 
 function getLocation(){
 	function locationSuccess(position){
-  console.log(position);
-  //return position;
  	getWeather(position);
  	getTaxon(position);
 	}
+	
 	function locationFailure(err){
-  $('.jsError').text(`ERROR(${err.code}): ${err.message}, Try entering a city.`);
+  $('.jsError').text(`ERROR(${err.code}): ${err.message} Try entering a city.`);
 	}
- 
-	if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(locationSuccess,locationFailure);
-  }else{
-  		$('.jsError').text(`Could not find location, try entering a city.`);
+ if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(locationSuccess,locationFailure);
+ 	}else{
+		$('.jsError').text(`Could not find location, try entering a city.`);
   };
 }
 
 function handleCityInput() {
 	$('.inputBar').submit( event => {
 		event.preventDefault();
-		$('.jsError').replaceWith(`<div class="jsError"></div>`);
+		$('.jsError').replaceWith(`<div class="jsError" aria-live="polite"></div>`);
 		var city = $('.cityInput').val();
 		getCityWeather(city);
 		});
@@ -32,20 +30,17 @@ function getWeather(position){
 	const longitude = position.coords.longitude;
 	
 	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&APPID=${APIKey}`)
-	  .then(response => {
+		.then(response => {
       if (response.ok){
       	return response.json();
-       //console.log( response.json());
-    		}
-      else {
+   }else {
      throw new Error (response.statusText);}
-    })
-    .then(responseJson => {
+   })
+   .then(responseJson => {
 			displayWeather(responseJson);
 			console.log(responseJson);
-			})
-			
-    .catch(err => { $('.jsError').text(`Something went wrong getting weather data: ${err.message}`);
+		})
+		.catch(err => { $('.jsError').text(`Something went wrong getting weather data: ${err.message}`);
     });
 }
 
@@ -56,62 +51,51 @@ function getCityWeather(city){
 	  .then(response => {
       if (response.ok){
       	return response.json();
-       //console.log( response.json());
-    		}
-      else {
-     throw new Error (response.statusText);}
-    })
-    .then(responseJson => {
+    		}else {
+					throw new Error (response.statusText);}
+				})
+   .then(responseJson => {
 				displayWeather(responseJson);
 				getCityTaxon(responseJson);
-				console.log(responseJson);
 			})
-
-    .catch(err => { $('.jsError').text(`Something went wrong getting weather for this city: ${err.message}`);
-    });
+   .catch(err => { $('.jsError').text(`Something went wrong getting weather for this city: ${err.message}`);
+   });
 }
     
 function getTaxon(position){
 	
 	const latitude = position.coords.latitude;
 	const longitude = position.coords.longitude;
-	
-	fetch(`https://api.inaturalist.org/v1/observations?geo=true&identified=true&photos=true&lat=${latitude}&lng=${longitude}&radius=5&per_page=12&order=desc&order_by=created_at`)
-	
+	fetch(`https://api.inaturalist.org/v1/observations?geo=true&identified=true&photos=true&lat=${latitude}&lng=${longitude}&radius=5&per_page=13&order=desc&order_by=created_at`)
 	.then(response => {
-    if (response.ok){
-      return response.json();
-    }
-    else {
+  	if (response.ok){
+     return response.json();
+   }else {
      throw new Error (response.statusText);}
     })
   .then(responseJson => {
   		displayTaxon(responseJson);
-  		console.log(responseJson);
   	})
   .catch(err => { $('.jsError').text(`Something went wrong getting sightings: ${err.message}`);
     });
-	}
+}
 	
 function getCityTaxon(responseJson){
 	
 	const latitude = responseJson.coord.lat;
 	const longitude = responseJson.coord.lon;
 	
-	fetch(`https://api.inaturalist.org/v1/observations?geo=true&identified=true&photos=true&lat=${latitude}&lng=${longitude}&radius=5&per_page=12&order=desc&order_by=created_at`)
-	
-	.then(response => {
+	fetch(`https://api.inaturalist.org/v1/observations?geo=true&identified=true&photos=true&lat=${latitude}&lng=${longitude}&radius=5&per_page=13&order=desc&order_by=created_at`)
+		.then(response => {
     if (response.ok){
       return response.json();
-    }
-    else {
+    }else {
      throw new Error (response.statusText);}
     })
-  .then(responseJson => {
-  displayTaxon(responseJson);
-  	console.log(responseJson);
+		.then(responseJson => {
+  		displayTaxon(responseJson);
   	})
-  .catch(err => { $('.jsError').text(`Something went wrong getting sightings: ${err.message}`);
+		.catch(err => { $('.jsError').text(`Something went wrong getting sightings: ${err.message}`);
     });
 	}
 	
@@ -126,7 +110,7 @@ function handleStart(){
 
 function displayWeather(responseJson){
 	$('.jsWeather').replaceWith(
-		`<div class="jsWeather" role="weather info">
+		`<div class="jsWeather" role="weather info" aria-live="polite">
 			<h1>Out There in ${responseJson.name}</h1>
 			<h2>${responseJson.weather[0].description.toUpperCase()}</h2>
 			<h2>Temperature: ${responseJson.main.temp}&#8457</h2>
@@ -142,18 +126,22 @@ function displayTaxon(responseJson){
 	for(let i=1; i< responseJson.results.length; i++){
 		if(responseJson.results[i].taxon.preferred_common_name === undefined){
 			$('.jsTaxon').append(
-			`<img alt="${responseJson.results[i].taxon.preferred_common_name}" src="${responseJson.results[i].photos[0].url}">
+			`<div class="resultsItem">
+			<img alt="${responseJson.results[i].taxon.preferred_common_name}" src="${responseJson.results[i].photos[0].url}">
 			<h2>Scientiffic Name: ${responseJson.results[i].taxon.name}</h2>
 			<h3>Observed: ${responseJson.results[i].observed_on} at ${responseJson.results[i].place_guess}</h3>
-			<h3><a href="${responseJson.results[i].taxon.wikipedia_url}" target="blank">More Information</a> </h3>`);
+			<h3><a href="${responseJson.results[i].taxon.wikipedia_url}" target="blank">More Information</a> </h3>
+			</div>`);
 		
 		}else{
 		$('.jsTaxon').append(
-			`<img alt="${responseJson.results[i].taxon.preferred_common_name}" src="${responseJson.results[i].photos[0].url}">
+			`<div class="resultsItem">
+			<img alt="${responseJson.results[i].taxon.preferred_common_name}" src="${responseJson.results[i].photos[0].url}">
 			<h2>${responseJson.results[i].taxon.preferred_common_name.toUpperCase()}</h2>
-			<h3>Scientiffic Name: ${responseJson.results[i].taxon.name}</h3>
+			<h3>Scientific Name: ${responseJson.results[i].taxon.name}</h3>
 			<h3>Observed: ${responseJson.results[i].observed_on} at ${responseJson.results[i].place_guess}</h3>
-			<h3><a href="${responseJson.results[i].taxon.wikipedia_url}" target="blank">More Information</a> </h3>`);
+			<h3><a href="${responseJson.results[i].taxon.wikipedia_url}" target="blank">More Information</a> </h3>
+			</div>`);
 		}
 	}
 }
@@ -164,4 +152,3 @@ function initialize(){
 }
 
 $(initialize())
-//getWeather(Position);
